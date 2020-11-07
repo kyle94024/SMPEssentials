b0932314f985e9ce980956f5c8e6268ea823dd6c
kyle94204 <kyle94204@gmail.com>
2020-11-06 17:11:47 -0800

    Fixed teleport command, added Pair.

--- src/main/java/com/gmail/kyle94024/smpessentials/utils/Teleport.java
+++ src/main/java/com/gmail/kyle94024/smpessentials/utils/Teleport.java
@@ -1,7 +1,7 @@
 package com.gmail.kyle94024.smpessentials.utils;
 
-
 import com.gmail.kyle94024.smpessentials.SMPEssentials;
+import com.gmail.kyle94024.smpessentials.data.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,14 +10,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.ChatColor;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Teleport implements CommandExecutor {
 
     // [0] => playerRequester [1] => playerTarget
-    private static Map<Player, Map.Entry<Player, Integer>> currentRequests = new HashMap<Player,Map.Entry<Player,Integer>>();
-    private Integer Id = Map<1>;
+    private static Map<Player, Pair<Player, Integer>> currentRequests = new HashMap<Player, Pair<Player, Integer>>();
     // { {Player player1, Player player2},{Player player3, PLayer player4} }
 
     @Override
@@ -44,8 +44,6 @@ public class Teleport implements CommandExecutor {
 
                 int keepAlive = 400;
 
-                sendRequest(player, targetPlayer);
-
                 int task = Bukkit.getScheduler().scheduleSyncDelayedTask((Plugin) SMPEssentials.getPlugin(), new Runnable() {
 
                     @Override
@@ -54,6 +52,8 @@ public class Teleport implements CommandExecutor {
 
                     }
                 }, keepAlive);
+
+                sendRequest(player, targetPlayer, task);
 
             } else {
                 player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Incorrect usage! Send a teleport request to a player.");
@@ -64,7 +64,9 @@ public class Teleport implements CommandExecutor {
         } else if (command.getName().equalsIgnoreCase("tpaccept")) {
             Player target = player;
             if (currentRequests.containsKey(target)) {
-                Player requester = currentRequests.get(target);
+                Player requester = currentRequests.get(target).getFirst();
+                int task = currentRequests.get(target).getSecond();
+                Bukkit.getScheduler().cancelTask(task);
                 currentRequests.remove(target);
                 if (requester.isOnline()) {
                     target.sendMessage(ChatColor.GREEN + "" + ChatColor.BOLD + "Teleport request from " + requester.getDisplayName() + " accepted.");
@@ -80,10 +82,8 @@ public class Teleport implements CommandExecutor {
                 return false;
             }
         } else if (command.getName().equalsIgnoreCase("tpdeny")) {
-            // TODO: Implement
             if (currentRequests.containsKey(player)){
                 killRequest(player,"denied");
-
                 return true;
             }
             else{
@@ -96,8 +96,8 @@ public class Teleport implements CommandExecutor {
 
 
 
-    public void sendRequest(Player requester, Player target) {
-        currentRequests.put(target,<requester,>);
+    public void sendRequest(Player requester, Player target, int task) {
+        currentRequests.put(target, new Pair<Player, Integer>(requester, task));
         requester.sendMessage(ChatColor.BLUE + "" + ChatColor.BOLD +"Sending a teleport request to " + target.getDisplayName() + ".");
 
         String[] messages = new String[3];
@@ -113,24 +113,29 @@ public class Teleport implements CommandExecutor {
     }
 
 
-    public boolean killRequest(Player target, String status ) {
+    public boolean killRequest(Player target, String status) {
         if (currentRequests.containsKey(target)) {
-            Player requester = currentRequests.get(target);
+            Player requester = currentRequests.get(target).getFirst();
+            int task = currentRequests.get(target).getSecond();
             if (requester.isOnline()) {
                 if (status.equals("expired")){
                     requester.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Your teleport request to " + target.getDisplayName() + " has expired.");
+
                 }
                 else if (status.equals("denied")){
                     requester.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "Your teleport request to " + target.getDisplayName() + " was denied.");
+                    Bukkit.getScheduler().cancelTask(task);
                 }
             }
             if (target.isOnline()) {
-                if(status.equals("expired")){
+                if (status.equals("expired")){
                     target.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + requester.getDisplayName() + "'s teleport request to you has expired.");
+
                 }
                else if(status.equals("denied")){
                    target.sendMessage((ChatColor.RED + "" + ChatColor.BOLD + "You have denied " + requester.getDisplayName() + "'s teleport request."));
-                }
+                   Bukkit.getScheduler().cancelTask(task);
+               }
             }
             currentRequests.remove(target);
             return true;
